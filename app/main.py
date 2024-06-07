daba2d1b549a0047416ddd9c3b640a4e65b134f1
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-06-07 01:17:15 -0300

    docusaurus pelo fastapi

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import Union, Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import RedirectResponse
+from starlette.staticfiles import StaticFiles
 
 from .routers import locations
 
@@ -17,7 +18,9 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 
-@app.get("/")
-def read_root():
+app.mount("/", StaticFiles(directory="docs/build", html=True), name="docs")
 
-    return RedirectResponse(url="/docs")
+
+@app.get("/openapi.json")
+def get_openapi():
+    return app.openapi()
